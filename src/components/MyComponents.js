dc1a489278cbafc8140e822f9cd73659a4d3f82c
Pham Duy Phuong <phuongphamgh@gmail.com>
2025-11-26 16:01:53 +0700

    26112025

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -6,14 +6,21 @@ class MyComponents extends React.Component {
         age: 26,
         address: "Can Tho"
     };
-
+    handleClick(event) {
+        console.log(">> Click my button ");
+        console.log("My name is ", this.state.name);
+        this.setState({
+            name: "Tuyen"
+        });
+    }
     /// JSX
     render() {
 
         return (
-            <p>
-                My address is {this.state.address} and I am {this.state.age} years old
-            </p>
+            <div>
+                My address is {this.state.address} and I am {this.state.age} years old, my name is {this.state.name}
+                <button onClick={(event) => { this.handleClick(event) }} >Click me</button>
+            </div>
 
         );
     }
